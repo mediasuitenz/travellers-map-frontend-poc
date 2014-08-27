cb824835125e7a3bf2e3c2b26909033993cf67d3
Richard Walker <digitalsadhu@gmail.com>
2014-08-27 22:27:27 +0200

    Add project js file

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var L          = require('./leaflet.js')
+  , level1Data = require('./data/level1-ways-weightings.json')
+  , level2Data = require('./data/level2-ways-weightings.json')
+  , level3Data = require('./data/level3-ways-weightings.json')
+  , level4Data = require('./data/level4-ways-weightings.json')
+
+
+var map = L.map('map')
+  .fitBounds([
+    [-43.577988,172.515934],
+    [-43.461397,172.749529]
+  ])
+
+L.Icon.Default.imagePath = '/images'
+
+// L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/digitalsadhu.jbf3mhe1/{z}/{x}/{y}.png', {
+  attribution: '',
+  maxZoom: 18
+}).addTo(map)
+
+function geojsonStyles(feature) {
+  var color = '#00FF00'
+  if (feature.properties.weighting < 8 && feature.properties.weighting > 3)
+    color = '#FFBF00'
+  if (feature.properties.weighting > 7) color = '#000000'
+  return {
+    color: color,
+    opacity: 1
+  }
+}
+
+var level1 = L.geoJson(level1Data, {
+  style: geojsonStyles
+})
+
+var level2 = L.geoJson(level2Data, {
+  style: geojsonStyles
+})
+
+var level3 = L.geoJson(level3Data, {
+  style: geojsonStyles
+})
+
+var level4 = L.geoJson(level4Data, {
+  style: geojsonStyles
+})
+
+map.setZoom(8)
+
+var currZoom = map.getZoom()
+map.on('zoomend', function () {
+  var newZoom = map.getZoom()
+  var inwards = newZoom > currZoom
+  var outwards = !inwards
+
+  if (inwards) {
+    if (newZoom === 10) level1.addTo(map)
+    if (newZoom === 12) level2.addTo(map)
+    if (newZoom === 14) level3.addTo(map)
+    if (newZoom === 16) level4.addTo(map)
+  }
+
+  if (outwards) {
+    if (newZoom === 9) level1.removeFrom(map)
+    if (newZoom === 11) level2.removeFrom(map)
+    if (newZoom === 13) level3.removeFrom(map)
+    if (newZoom === 15) level4.removeFrom(map)
+  }
+
+  currZoom = newZoom
+})
